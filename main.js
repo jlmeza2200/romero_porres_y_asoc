5b9ff802824419bbd2763350351ae9424357ade9
Joni <jonatanmeza01@gmail.com>
2022-12-01 23:18:37 -0300

    Upload project 100%

--- main.js
+++ main.js
@@ -40,19 +40,3 @@ const closeNav = () => {
 
 closeBtn.addEventListener("click", closeNav);
 
-// Collapse content
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
